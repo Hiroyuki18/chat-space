458ed732b53c093c19636534bca5b8338067437a
山田宏幸 <puramai200@gmail.com>
2020-02-14 20:21:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,10 +58,11 @@ $(function(){
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
-      $('.submit-btn').prop("disabled", false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
       $('.submit-btn').prop("disabled", false);
     });
     
