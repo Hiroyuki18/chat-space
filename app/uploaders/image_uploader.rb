edccf96640c67868669552f13452ce7a3a430c75
山田宏幸 <puramai200@gmail.com>
2020-02-21 16:04:36 +0900

    更新

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -24,6 +24,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   process resize_to_fit: [800, 800]
+
+  
   #
   # def scale(width, height)
   #   # do something
